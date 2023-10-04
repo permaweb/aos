395cb14c13ee40e3f2ddbd8d115c5cba530e219e
Tom Wilson <tom@hyper.io>
2023-10-04 16:33:15 +0000

    updated create contract service

--- src/services/create-contract.js
+++ src/services/create-contract.js
@@ -4,6 +4,6 @@ import { fromPromise } from 'hyper-async'
 export function createContract({ wallet, src, initState }) {
   const signer = sdk.createDataItemSigner(wallet)
   return fromPromise(() => sdk.createContract(src, initState, signer, []))()
-    .map(res => (console.log(res), res))
+
 
 }
