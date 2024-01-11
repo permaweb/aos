90530fd20416a73b2b258028dd6af06a6e34991c
Tom Wilson <tom@hyper.io>
2024-01-11 21:46:20 +0000

    fix: issue when creating process it could not be found - added a 1 second wait

--- src/services/spawn-process.js
+++ src/services/spawn-process.js
@@ -7,6 +7,8 @@ export function spawnProcess({ wallet, src, tags }) {
 
   return fromPromise(() => connect().spawn({
     module: src, scheduler: 'TZ7o7SIZ06ZEJ14lXwVtng1EtSx60QkPy-kh-kdAXog', signer, tags
-  }))()
+  })
+    .then(result => new Promise((resolve) => setTimeout(() => resolve(result), 1000)))
+  )()
 
 }
