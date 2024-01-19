9693da6cb8bd40a39843e57e0b7ceb6e277904d2
Tom Wilson <tom@hyper.io>
2024-01-19 13:08:18 +0000

    refactor: stringify format to use pretty print #59

--- process/stringify.lua
+++ process/stringify.lua
@@ -2,43 +2,47 @@
 
 local stringify = { _version = "0.0.1" }
 
-function stringify.format(tbl)
+function stringify.format(tbl, indent)
+  indent = indent or 0
+  local toIndent = string.rep(" ", indent)
+  local toIndentChild = string.rep(" ", indent + 2)
+
   local result = {}
   local isArray = true
   local arrayIndex = 1
 
   for k, v in pairs(tbl) do
-      if isArray then
-          if k == arrayIndex then
-              arrayIndex = arrayIndex + 1
-              if type(v) == "table" then
-                v = stringify.format(v)
-              elseif type(v) == "string" then
-                v = '"' .. v .. '"'
-              else 
-                v = tostring(v)
-              end
-              table.insert(result, v)
-          else
-              isArray = false
-              result = {}
-          end
+    if isArray then
+      if k == arrayIndex then
+        arrayIndex = arrayIndex + 1
+        if type(v) == "table" then
+          v = stringify.format(v, indent + 2)
+        elseif type(v) == "string" then
+          v = '"' .. v .. '"'
+        else
+          v = tostring(v)
+        end
+        table.insert(result, toIndentChild .. v)
+      else
+        isArray = false
+        result = {}
       end
-      if not isArray then
-          if type(v) == "table" then
-            v = stringify.format(v)
-          elseif type(v) == "string" then
-            v = '"' .. v .. '"'
-          else 
-            v = tostring(v)
-          end
-          table.insert(result, k .. " = " .. v)
+    end
+    if not isArray then
+      if type(v) == "table" then
+        v = stringify.format(v, indent + 2)
+      elseif type(v) == "string" then
+        v = '"' .. v .. '"'
+      else
+        v = tostring(v)
       end
+      table.insert(result, toIndentChild .. k .. " = " .. v)
+    end
   end
 
-  local prefix = isArray and "{" or "{ "
-  local suffix = isArray and "}" or " }"
-  local separator = isArray and "," or ", "
+  local prefix = isArray and "{\n" or "{\n "
+  local suffix = isArray and "\n" .. toIndent .. " }" or "\n" .. toIndent .. "}"
+  local separator = isArray and ",\n" or ",\n "
   return prefix .. table.concat(result, separator) .. suffix
 end
 
