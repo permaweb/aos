03fec20fd6897c67d74cb5734eb5ac01ef8546df
Marton Lederer <marton@lederer.hu>
2024-01-24 10:23:16 +0100

    feat: support absolute paths for ".load"

--- src/commands/load.js
+++ src/commands/load.js
@@ -6,11 +6,15 @@ export function load(line) {
   // get filename
   let fn = line.split(' ')[1]
   if (/\.lua$/.test(fn)) {
-    if (!fs.existsSync(path.resolve(process.cwd() + '/' + fn))) {
+    let filePath = fn;
+    if (!path.isAbsolute(filePath)) {
+      filePath = path.resolve(path.join(process.cwd(), fn))
+    }
+    if (!fs.existsSync(filePath)) {
       throw Error(chalk.red('ERROR: file not found.'));
     }
     console.log(chalk.green('Loading... ', fn));
-    line = fs.readFileSync(path.resolve(process.cwd() + '/' + fn), 'utf-8');
+    line = fs.readFileSync(filePath, 'utf-8');
     return line
   } else {
     throw Error(chalk.red('ERROR: .load function requires a *.lua file'))
