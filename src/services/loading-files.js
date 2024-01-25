2bb22ab86e9b26b34b9f78333e461afe2fda682e
Marton Lederer <marton@lederer.hu>
2024-01-25 11:15:02 +0100

    feat: building project structures

--- src/services/loading-files.js
+++ src/services/loading-files.js
@@ -1,4 +1,5 @@
 import minimist from 'minimist'
+import fs from 'fs'
 
 export function checkLoadArgs() {
   const argv = minimist(process.argv.slice(2))
@@ -9,4 +10,39 @@ export function checkLoadArgs() {
     return argv.load
   }
   return []
+}
+
+/**
+ * Create the project structure from the main file's content
+ */
+export function createProjectStructure(mainFile) {
+  const modules = findRequires(mainFile)
+
+  for (let i = 0; i < modules.length; i++) {
+    if (modules[i].content) continue
+
+    modules[i].content = fs.readFileSync(modules[i].path, 'utf-8')
+
+    const requiresInMod = findRequires(modules[i].content)
+    requiresInMod.forEach((mod) => {
+      if (modules.find((m) => m.name === mod.name)) return
+      modules.push(mod)
+    })
+  }
+}
+
+export function findRequires(data) {
+  const requirePattern = /(?<=(require( *)(\n*)(\()?( *)("|'))).*(?=("|'))/g
+  const requiredModules = data.match(requirePattern).map(
+    (mod) => ({
+      name: mod,
+      path: path.join(
+        process.cwd(),
+        mod.replace(/\./g, '/') + '.lua'
+      ),
+      content: undefined
+    })
+  )
+
+  return requiredModules
 }
