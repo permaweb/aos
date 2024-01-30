a560a1c6b304fd7f789fae756db31d8091fa38b2
Tom Wilson <tom@hyper.io>
2024-01-30 17:58:11 +0000

    fix: update mu and cu params #104

--- src/services/read-result.js
+++ src/services/read-result.js
@@ -4,10 +4,10 @@ import { fromPromise, Resolved, Rejected } from 'hyper-async'
 export function readResult(params) {
   const info = {}
   if (process.env.CU_URL) {
-    info['CU_URL'] = process.env.CU_URL
+    info['CU_URL'] = process.env.CU_URL || 'https://cu.ao-testnet.xyz'
   }
   if (process.env.MU_URL) {
-    info['MU_URL'] = process.env.MU_URL
+    info['MU_URL'] = process.env.MU_URL || 'https://mu.ao-testnet.xyz'
   }
   return fromPromise(() => connect(info).result(params))()
     // log the error messages most seem related to 503
