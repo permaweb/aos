478fa17ed546825472b63e8fd4b31083b88a81ba
Tom Wilson <tom@hyper.io>
2024-01-30 17:55:05 +0000

    update all CU and MU Checks

--- src/services/send-message.js
+++ src/services/send-message.js
@@ -5,10 +5,10 @@ export function sendMessage({ processId, wallet, tags, data }) {
   const signer = createDataItemSigner(wallet)
   const info = {}
   if (process.env.CU_URL) {
-    info['CU_URL'] = process.env.CU_URL
+    info['CU_URL'] = process.env.CU_URL || 'https://cu.ao-testnet.xyz'
   }
   if (process.env.MU_URL) {
-    info['MU_URL'] = process.env.MU_URL
+    info['MU_URL'] = process.env.MU_URL || 'https://mu.ao-testnet.xyz'
   }
   return fromPromise(() => connect(info).message({ process: processId, signer, tags, data }))()
   //.map(result => (console.log(result), result))
