e00d183a90574468803a432447f598838624ed50
Tom Wilson <tom@hyper.io>
2024-02-16 01:49:59 +0000

    refactor: add support for windows thx vinceJ for the help

--- bin/aos.js
+++ bin/aos.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 import url from 'url'
 import path from 'node:path'
+import os from 'node:os'
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+let __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
-import(path.resolve(__dirname + '../src/index.js'))
+if (os.platform() === 'win32') {
+  __dirname = __dirname.replace(/\\/g, "/").replace(/^[A-Za-z]:\//, "/")
+  import(__dirname + '../src/index.js')
+} else {
+  import(path.resolve(__dirname + '../src/index.js'))
+}
