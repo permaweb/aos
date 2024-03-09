0e91186a26a3b19764cd963e66697fc43a950f05
Vedant Chainani <vedantchainani1084@gmail.com>
2024-03-10 00:17:02 +0530

    chore: add sha1 digest

--- process/crypto/init.lua
+++ process/crypto/init.lua
@@ -5,6 +5,8 @@ local Stream = require(".crypto.util.stream")
 local MD2 = require(".crypto.digest.md2")
 local MD4 = require(".crypto.digest.md4")
 local MD5 = require(".crypto.digest.md5")
+local SHA1 = require(".crypto.digest.sha1")
+
 local utils = {
     stream = Stream
 }
@@ -12,7 +14,8 @@ local utils = {
 local digest = {
     md2 = MD2,
     md4 = MD4,
-    md5 = MD5
+    md5 = MD5,
+    sha1 = SHA1
 }
 
 local crypto = {
