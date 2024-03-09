5ef7325881de7f6274904fa21ca5c18dd4c8c5c6
Vedant Chainani <vedantchainani1084@gmail.com>
2024-03-10 00:10:24 +0530

    chore: add md5 digest

--- process/crypto/init.lua
+++ process/crypto/init.lua
@@ -4,13 +4,15 @@ local Stream = require(".crypto.util.stream")
 -- Digests
 local MD2 = require(".crypto.digest.md2")
 local MD4 = require(".crypto.digest.md4")
+local MD5 = require(".crypto.digest.md5")
 local utils = {
     stream = Stream
 }
 
 local digest = {
     md2 = MD2,
-    md4 = MD4
+    md4 = MD4,
+    md5 = MD5
 }
 
 local crypto = {
