4a7aad332d8556fb0da8ba51753000a22117df0c
Vedant Chainani <vedantchainani1084@gmail.com>
2024-03-10 16:48:50 +0530

    chore: add string to hex encode utility

---
+++ process/crypto/util/hex.lua
@@ -0,0 +1,26 @@
+-- Converts a string to its hexadecimal representation.
+-- If the string is empty, it returns an empty string.
+-- If the 'ln' parameter is not provided, it performs the conversion without adding newlines or separators.
+-- If the 'ln' parameter is provided, it adds a newline character every 'ln' bytes.
+-- The 'sep' parameter is an optional separator between each byte.
+-- Returns the hexadecimal representation of the string.
+local function stringToHex(s, ln, sep)
+	if #s == 0 then return "" end
+	if not ln then
+		return (s:gsub('.',
+			function(c) return string.format('%02x', string.byte(c)) end
+		))
+	end
+	sep = sep or ""
+	local t = {}
+	for i = 1, #s - 1 do
+		t[#t + 1] = string.format("%02x%s", s:byte(i),
+			(i % ln == 0) and '\n' or sep)
+	end
+	t[#t + 1] = string.format("%02x", s:byte(#s))
+	return table.concat(t)
+end
+
+return {
+	stringToHex = stringToHex,
+}
