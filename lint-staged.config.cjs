e20554c4594bf826decfb6c0a69378bf9fb682cf
Jack Frain <jfrain99@gmail.com>
2024-11-04 14:43:06 -0500

    chore(aos): add stylua

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -2,6 +2,22 @@ module.exports = {
   '**/*.js': [
     'standard --fix'
   ],
+  '**/*.lua': (allFiles) => {
+    const styluaArgs = allFiles.reduce((acc, file) => {
+      const path = "**" + file.replace(process.cwd(), '')
+      return acc + ` -g ${path}`
+    }, '')
+
+    const luacheckArgs = allFiles.reduce((acc, file) => {
+      const path = "." + file.replace(process.cwd(), '')
+      return acc + ` ${path}`
+    }, '')
+
+    return [
+      `stylua ${styluaArgs} -- .`,
+      `luacheck --config .luacheckrc ${luacheckArgs}`,
+    ]
+  },
   '**/package.json': [
     'sort-package-json'
   ],
