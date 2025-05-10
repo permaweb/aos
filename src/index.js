9a35e0d3dc4b93f7fc96bfbaa8fcadf953879574
rakis <tom@hyper.io>
2025-05-09 20:23:18 -0400

    fix: updated aoconnect to 84

--- src/index.js
+++ src/index.js
@@ -542,7 +542,7 @@ async function connect(jwk, id) {
   for (let i = 0; i < 50; i++) {
     if (_prompt === undefined) {
       spinner.suffixText = chalk.red('[Connecting to process....]')
-      await new Promise(resolve => setTimeout(resolve, 500 * i))
+      // await new Promise(resolve => setTimeout(resolve, 10 * i))
       promptResult = await evaluate("require('.process')._version", id, jwk, { sendMessage, readResult }, spinner)
       // console.log({ promptResult })
       _prompt = promptResult?.Output?.prompt || promptResult?.Output?.data?.prompt
