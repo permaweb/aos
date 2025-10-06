25316fbaa1bb41b7adc69ab34203f3053a44de23
rakis <tom@hyper.io>
2025-10-06 19:52:33 -0400

    chore: revert stack check

--- src/services/mainnet.js
+++ src/services/mainnet.js
@@ -142,9 +142,10 @@ export function spawnProcessMainnet({ wallet, src, tags, data, isHyper }) {
     device: 'process@1.0',
     'scheduler-device': 'scheduler@1.0',
     'push-device': 'push@1.0',
-    'execution-device': 'stack@1.0',
-    'device-stack/1': 'dedup@1.0',
-    'device-stack/2': 'lua@5.3a',
+    'execution-device': 'lua@5.3a',
+    // 'execution-device': 'stack@1.0',
+    // 'device-stack/1': 'dedup@1.0',
+    // 'device-stack/2': 'lua@5.3a',
     'data-protocol': 'ao',
     variant: 'ao.N.1',
     ...tags.reduce((a, t) => assoc(t.name.toLowerCase(), t.value, a), {}),
@@ -159,7 +160,8 @@ export function spawnProcessMainnet({ wallet, src, tags, data, isHyper }) {
     .chain(setAuthority)
     .chain(params => isHyper ? of(params) : getExecutionDevice(params))
     .map(p => {
-      if (p['execution-device'] === 'stack@1.0') {
+      //if (p['execution-device'] === 'stack@1.0') {
+      if (p['execution-device'] === 'lua@5.3a') {
         p.module = process.env.AOS_MODULE || pkg.hyper.module
       } else {
         p.module = src
