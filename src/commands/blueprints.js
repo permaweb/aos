f50618572115033e1a445cffb5d2906e79508d37
Nick Juliano <nickjuliano20@gmail.com>
2026-06-19 09:16:52 -0400

    fix: bundling moved runtime code from src to dist

--- src/commands/blueprints.js
+++ src/commands/blueprints.js
@@ -1,14 +1,12 @@
 import fs from 'fs'
 import path from 'path'
-import * as url from 'url'
+import { packageRoot } from '../package-root.js'
 import { chalk } from '../utils/colors.js'
-
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 export function loadBlueprint(line) {
   let name = line.split(' ')[1]
-  const luaFile = __dirname + '../../blueprints/' + name + '.lua'
-  if (fs.existsSync(path.resolve(luaFile))) {
+  const luaFile = path.join(packageRoot, 'blueprints', `${name}.lua`)
+  if (fs.existsSync(luaFile)) {
     const code = fs.readFileSync(luaFile, 'utf-8')
     console.log(chalk.green('Loading... ', name))
     return code
--- src/services/get-pkg.js
+++ src/services/get-pkg.js
@@ -1,15 +1,8 @@
 import fs from 'node:fs'
 import path from 'node:path'
-import os from 'node:os'
-import * as url from 'url'
+import { packageRoot } from '../package-root.js'
 
-let __dirname = url.fileURLToPath(new URL('.', import.meta.url))
-
-if (os.platform() === 'win32') {
-  __dirname = __dirname.replace(/\\/g, '/')
-}
-
-const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname + '../../package.json')))
+const pkg = JSON.parse(fs.readFileSync(path.join(packageRoot, 'package.json'), 'utf-8'))
 
 export function getPkg() {
   return pkg
